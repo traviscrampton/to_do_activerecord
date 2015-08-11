82d3c58b719ae3853aff3a209092aeccc218e838
Jake Glassman & Justin Scott <student@epicodus.com>
2015-08-11 09:30:21 -0700

    Have complete specs

--- lib/to_do.rb
+++ lib/to_do.rb
@@ -1,4 +1,6 @@
 class Task
+  @@all_tasks = []
+
   define_method(:initialize) do |description|
     @description = description
   end
@@ -8,6 +10,14 @@ class Task
   end
 
   define_singleton_method(:all) do
-    []
+    @@all_tasks
+  end
+
+  define_method(:save) do
+    @@all_tasks.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_tasks =[]
   end
 end
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -2,6 +2,8 @@ require('rspec')
 require('to_do')
 
 describe(Task) do
+
+
   describe('#description') do
     it("gives description") do
       test_task=Task.new("wash the dog")
@@ -13,5 +15,25 @@ describe(Task) do
     it("is empty at first") do
       expect(Task.all()).to(eq([]))
     end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new("clean the porch")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties saved tasks") do
+      Task.new("wash the car").save()
+      Task.clear()
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  before() do
+    Task.clear()
   end
 end
